1b7cc4680192f193781fa45b97a0abf557f08932
cSDes1gn <christian911@sympatico.ca>
2021-01-09 19:01:57 -0500

    calibration metrics

--- include/CSS/src/Css.h
+++ include/CSS/src/Css.h
@@ -25,6 +25,10 @@ class Css {
         uint16_t read();
     
     private:
-        uint16_t adc;
+        // voltage levels calibrated by sensor
+        const uint16_t av = 620; // voltage reading in air
+        const uint16_t wv = 310; // voltage reading H20 submerged
+        uint16_t smv = 0; // analog voltage reading
+        uint16_t smp = 0; // soil moisture as a percentage
 };
 #endif  // INCLUDE_CSS_SRC_CSS_H_
