324c11fb30e49b4f048e172aaf8f472996e32534
cSDes1gn <christian911@sympatico.ca>
2021-01-09 21:52:53 -0500

    lumex package for luminous flux sensor

---
+++ include/Lumex/Package.h
@@ -0,0 +1,11 @@
+// -*- lsst-c++ -*-
+/**
+ * @defgroup Lumex
+ *
+ * @brief Interface for Luminous flux senso
+ * 
+ * Copyright © 2020 OpenHerb.
+ */
+#ifndef INCLUDE_LUMEX_PACKAGE_H_
+#define INCLUDE_LUMEX_PACKAGE_H_
+#endif  // INCLUDE_LUMEX_PACKAGE_H_
