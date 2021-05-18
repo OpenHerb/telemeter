5a6ac9b6e4ce460011cbdab4f71ee5387e3aa932
cSDes1gn <christian@leapsystems.online>
2021-05-18 03:03:28 +0000

    move cyclic buffer to private member

--- include/CSS/src/Css.h
+++ include/CSS/src/Css.h
@@ -24,21 +24,21 @@ class Css {
     public:
         // CSS constructor specification
         struct Spec {
-            uint8_t css_pin;        // analog pin for css sensor
-            uint8_t buffer_size;    // size of cyclic buffer for averaging
-            uint16_t av;            // voltage reading in air
-            uint16_t sv;            // voltage reading H20 submerged
+            uint8_t css_pin;            // analog pin for css sensor
+            uint8_t buffer_size;        // size of cyclic buffer for averaging
+            uint16_t av;                // voltage reading in air
+            uint16_t sv;                // voltage reading H20 submerged
         };
-        CyclicBuffer<uint8_t>* buffer;
         explicit Css(Spec spec);
         ~Css();
         uint8_t read();
     
     private:
-        uint16_t av = 620;          // voltage calibrated to air
-        uint16_t sv = 310;          // voltage calibrated submerged
-        uint8_t pin;                // css pin
-        uint16_t smv = 0;           // analog voltage reading
-        uint8_t smp = 0;            // soil moisture as a percentage
+        CyclicBuffer<uint8_t>* buffer;
+        uint16_t av = 620;              // voltage calibrated to air
+        uint16_t sv = 310;              // voltage calibrated submerged
+        uint8_t pin;                    // css pin
+        uint16_t smv = 0;               // analog voltage reading
+        uint8_t smp = 0;                // soil moisture as a percentage
 };
 #endif  // INCLUDE_CSS_SRC_CSS_H_
