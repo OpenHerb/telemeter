2191f03971baa906e42964e6dae5cd5c855f2a02
cSDes1gn <christian@leapsystems.online>
2021-05-19 04:50:02 +0000

    define telemeter typedefs in global header

---
+++ include/Types/Types.h
@@ -0,0 +1,23 @@
+/**
+ * @file Types.h
+ * @author Christian Sargusingh (christian@leapsystems.online)
+ * @brief Telemeter Project typedefs
+ * @version 0.1
+ * @date 2021-05-19
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef INCLUDE_TYPES_TYPES_H_
+#define INCLUDE_TYPES_TYPES_H_
+
+typedef struct Sensorframe {
+    uint8_t sm;         // soil moisture (%)
+    uint16_t lx;        // luminous flux (lumens)
+    uint8_t tp;         // ambient temperature (˚C)
+    uint8_t pa;         // ambient pressure (kPa)
+    uint8_t rh;         // relative humidity (%)
+};
+
+#endif // INCLUDE_TYPES_TYPES_H_ 
